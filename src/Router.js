7277627f3dfaea32f831c9c0497df8ff2779bce7
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-01-12 16:03:06 +0900

    Add: secound commit. 초기 세팅 완료.

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,26 @@
+import { Route, Router } from 'react-router-dom';
+
 import MainSumin from './pages/Sumin/Main/Main';
 import LoginSumin from './pages/Sumin/Login/Login';
-
-<Route path='/main-sumin' element={<MainSumin />} />
-<Route path='/login-sumin' element={<LoginSumin />} />
-
 import MainHwajong from './pages/Hwajong/Main/Main';
 import LoginHwajong from './pages/Hwajong/Login/Login';
-
-<Route path='/login-Hwajong' element={<LoginHwajong />} />
-<Route path='/main-Hwajong' element={<MainHwajong />} />
-
 import MainJieun from './pages/Jieun/Main/Main';
 import LoginJieun from './pages/Jieun/Login/Login';
-
-<Route path='/login-Jieun' element={<LoginJieun />} />
-<Route path='/main-Jieun' element={<MainJieun />} />
-
 import MainPkw from './pages/Pkw/Main/Main';
 import LoginPkw from './pages/Pkw/Login/Login';
 
-<Route path='/login-Pkw' element={<LoginPkw />} />
-<Route path='/main-Pkw' element={<MainPkw />} />
+<Router>
+  <Route path="/Main" element={<MainSumin />} />
+  <Route path="/Login" element={<LoginSumin />} />
+
+  <Route path="/Hwajong" element={<LoginHwajong />} />
+  <Route path="/Hwajong" element={<MainHwajong />} />
+
+  <Route path="/Jieun" element={<LoginJieun />} />
+  <Route path="/Jieun" element={<MainJieun />} />
+
+  <Route path="/Pkw" element={<LoginPkw />} />
+  <Route path="/Pkw" element={<MainPkw />} />
+</Router>;
+
+export default Router;

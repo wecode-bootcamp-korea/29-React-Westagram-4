af79b9de42c554fcb0ddfd91b7fb9685ed0ca56a
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-01-12 15:20:58 +0900

    Add: first commit. 초기 세팅 완료.

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import MainSumin from './pages/Sumin/Main/Main';
+import LoginSumin from './pages/Sumin/Login/Login';
+
+<Route path='/main-sumin' element={<MainSumin />} />
+<Route path='/login-sumin' element={<LoginSumin />} />
+
+import MainHwajong from './pages/Hwajong/Main/Main';
+import LoginHwajong from './pages/Hwajong/Login/Login';
+
+<Route path='/login-Hwajong' element={<LoginHwajong />} />
+<Route path='/main-Hwajong' element={<MainHwajong />} />
+
+import MainJieun from './pages/Jieun/Main/Main';
+import LoginJieun from './pages/Jieun/Login/Login';
+
+<Route path='/login-Jieun' element={<LoginJieun />} />
+<Route path='/main-Jieun' element={<MainJieun />} />
+
+import MainPkw from './pages/Pkw/Main/Main';
+import LoginPkw from './pages/Pkw/Login/Login';
+
+<Route path='/login-Pkw' element={<LoginPkw />} />
+<Route path='/main-Pkw' element={<MainPkw />} />

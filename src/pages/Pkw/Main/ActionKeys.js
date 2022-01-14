c88764361f4f16680aa0ecba2e6541e468a34880
pkw-May <pkw.may@gmail.com>
2022-01-14 17:43:10 +0900

    Modify: followed eslint

--- src/pages/Pkw/Main/ActionKeys.js
+++ src/pages/Pkw/Main/ActionKeys.js
@@ -2,12 +2,27 @@ function ActionKeys() {
   return (
     <div className="action-keys">
       <div className="action-left-keys">
-        <img className="icon" id="like" src="/images/Pkw/like.png" />
-        <img className="icon" id="comment" src="/images/Pkw/chat.png" />
-        <img className="icon" id="share" src="/images/Pkw/upload.png" />
+        <img className="icon" id="like" alt="like" src="/images/Pkw/like.png" />
+        <img
+          className="icon"
+          id="comment"
+          alt="comment"
+          src="/images/Pkw/chat.png"
+        />
+        <img
+          className="icon"
+          id="share"
+          alt="share"
+          src="/images/Pkw/upload.png"
+        />
       </div>
       <div className="action-right-keys">
-        <img className="icon" id="bookmark" src="/images/Pkw/bookmark.png" />
+        <img
+          className="icon"
+          id="bookmark"
+          alt="bookmark"
+          src="/images/Pkw/bookmark.png"
+        />
       </div>
     </div>
   );

a145804efac85f421721550b3c151bd5c51ac4d2
oh-sumin <osumin740@gmail.com>
2022-01-14 20:19:54 +0900

    common때문에 억지로

--- src/Router.js
+++ src/Router.js
@@ -1,26 +1,29 @@
-import { Route, Router } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainSumin from './pages/Sumin/Main/Main';
 import LoginSumin from './pages/Sumin/Login/Login';
-import MainHwajong from './pages/Hwajong/Main/Main';
-import LoginHwajong from './pages/Hwajong/Login/Login';
-import MainJieun from './pages/Jieun/Main/Main';
-import LoginJieun from './pages/Jieun/Login/Login';
-import MainPkw from './pages/Pkw/Main/Main';
-import LoginPkw from './pages/Pkw/Login/Login';
+// import MainHwajong from './pages/Hwajong/Main/Main';
+// import LoginHwajong from './pages/Hwajong/Login/Login';
+// import MainJieun from './pages/Jieun/Main/Main';
+// import LoginJieun from './pages/Jieun/Login/Login';
+// import MainPkw from './pages/Pkw/Main/Main';
+// import LoginPkw from './pages/Pkw/Login/Login';
 
-<Router>
-  <Route path="/Main" element={<MainSumin />} />
-  <Route path="/Login" element={<LoginSumin />} />
-
-  <Route path="/Hwajong" element={<LoginHwajong />} />
-  <Route path="/Hwajong" element={<MainHwajong />} />
-
-  <Route path="/Jieun" element={<LoginJieun />} />
-  <Route path="/Jieun" element={<MainJieun />} />
-
-  <Route path="/Pkw" element={<LoginPkw />} />
-  <Route path="/Pkw" element={<MainPkw />} />
-</Router>;
-
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/main" element={<MainSumin />} />
+        <Route path="/" element={<LoginSumin />} />
+        {/* <Route path="/Hwajong" element={<LoginHwajong />} />
+        <Route path="/Hwajong" element={<MainHwajong />} />
+        <Route path="/Jieun" element={<LoginJieun />} />
+        <Route path="/Jieun" element={<MainJieun />} />
+        <Route path="/Pkw" element={<LoginPkw />} />
+        <Route path="/Pkw" element={<MainPkw />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default Router;
--- src/pages/Sumin/Login/Login.js
+++ src/pages/Sumin/Login/Login.js
@@ -1,28 +1,34 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.scss';
 
+const Login = () => {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/Main');
+  };
 
+  return (
+    <section>
+      <div className="box">
+        <div className="logo">westagram</div>
+        <form className="login-form">
+          <input
+            className="id input-style"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            className="pw input-style"
+            type="password"
+            placeholder="비밀번호"
+          />
+          <button onClick={goToMain}>로그인</button>
+        </form>
+        <div className="forgot-password">비밀번호를 잊으셨나요?</div>
+      </div>
+    </section>
+  );
+};
 
-function Login() {
-    const navigate = useNavigate();
-    const goToMain = () => {
-        navigate("/main")
-    };
-
-    return(
-        <section>
-        <div className="box">
-            <div className="logo">westagram</div>
-                <form className="login-form">
-                    <input className="id input-style" type="text" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-                    <input className="pw input-style" type="password" placeholder="비밀번호"/>
-                    <button onClick={goToMain}>로그인</button>
-                </form>
-            <div className="forgot-password">비밀번호를 잊으셨나요?</div>
-        </div>
-        </section>
-    )
-}
-
-export default LoginSumin;
+export default Login;

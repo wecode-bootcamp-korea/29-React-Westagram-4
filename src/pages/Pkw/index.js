08614e5764e985447cf75d7b793dcbba2b2f01b9
pkw-May <pkw.may@gmail.com>
2022-01-14 16:28:12 +0900

    Add: entry src

---
+++ src/pages/Pkw/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from './Router';
+import index from './index.scss'
+
+ReactDOM.render(
+  <Router />, document.getElementById('root')
+);

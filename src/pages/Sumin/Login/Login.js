a91199398180861a4f89be09c8a48aa39d0edf5e
pkw-May <pkw.may@gmail.com>
2022-01-17 15:50:01 +0900

    Fix: initial rollback

--- src/pages/Sumin/Login/Login.js
+++ src/pages/Sumin/Login/Login.js
@@ -1,34 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Login.scss';
+import React from 'react'
 
 const Login = () => {
-  const navigate = useNavigate();
-  const goToMain = () => {
-    navigate('/Main');
-  };
+    return (
+        <div>
+            
+        </div>
+    )
+}
 
-  return (
-    <section>
-      <div className="box">
-        <div className="logo">westagram</div>
-        <form className="login-form">
-          <input
-            className="id input-style"
-            type="text"
-            placeholder="전화번호, 사용자 이름 또는 이메일"
-          />
-          <input
-            className="pw input-style"
-            type="password"
-            placeholder="비밀번호"
-          />
-          <button onClick={goToMain}>로그인</button>
-        </form>
-        <div className="forgot-password">비밀번호를 잊으셨나요?</div>
-      </div>
-    </section>
-  );
-};
-
-export default Login;
+export default Login

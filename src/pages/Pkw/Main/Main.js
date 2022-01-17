0d063d4163b16712a40115772b45bf7be6ad34d6
홍쥐 <ghdwl@hongjwiui-MacBookPro.local>
2022-01-17 15:53:34 +0900

    rollback

--- src/pages/Pkw/Main/Main.js
+++ src/pages/Pkw/Main/Main.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import main from './Main.scss';
-import Article from './Article';
-import RightMenu from './RightMenu';
-import Nav from '../../../components/Nav/Nav';
+import React from 'react'
 
-function Main() {
-  return (
-    <div className="main-area">
-      <Nav />
-      <main>
-        <Article />
-        <RightMenu />
-      </main>
-    </div>
-  );
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
-export default Main;
+export default Main

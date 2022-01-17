93e3d2b6fca961cf740b24a93d7f7c51de4a36e4
oh-sumin <osumin740@gmail.com>
2022-01-17 15:50:10 +0900

    fix 이전 작업물 수정

--- src/pages/Pkw/Login/Login.js
+++ src/pages/Pkw/Login/Login.js
@@ -1,34 +1,11 @@
-import React from 'react';
-import login from './Login.scss';
-import { useNavigate, Link } from 'react-router-dom';
+import React from 'react'
 
-function Login(props) {
-  const navigate = useNavigate();
-  const goToMain = () => {
-    navigate('/pkw-main');
-  };
-
-  return (
-    <div className="login-box">
-      <p className="logo">westagram</p>
-      <div className="login-data">
-        <input
-          type="text"
-          placeholder="전화번호, 사용자 이름 또는 이메일"
-          // value={props.id}
-        />
-        <input
-          type="password"
-          placeholder="비밀번호"
-          // value={props.pw}
-        />
-        <button onClick={goToMain}>로그인</button>
-      </div>
-      <Link to="" className="info">
-        비밀번호를 잊으셨나요?
-      </Link>
-    </div>
-  );
+const Login = () => {
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
-export default Login;
+export default Login

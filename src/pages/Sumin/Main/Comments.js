29b28d044d5a85d9fdb2b9405f43a0aaeb0e1c7c
oh-sumin <osumin740@gmail.com>
2022-01-18 21:08:25 +0900

    input 입력값 state에 저장 upload button Validation button event preventDefault()

--- src/pages/Sumin/Main/Comments.js
+++ src/pages/Sumin/Main/Comments.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.scss';
+import InputComments from '../../Sumin/Main/InputComments';
 
 function Comments() {
+  const [comment, setComment] = useState('');
+  const [isUploadBtn, setIsUploadBtn] = useState(false);
+
+  const handleCommentInput = event => {
+    setComment(event.target.value);
+  };
+
+  const upLoadBtnValidation = () => {
+    return comment.length > 0 ? setIsUploadBtn(true) : setIsUploadBtn(false);
+  };
+
+  function uploadComment(event) {
+    event.preventDefault();
+  }
+
+  //console.log(isUploadBtn);
+
   return (
-    <div class="feed-comment">
-      <ul class="comment-box"></ul>
-      <form class="reply">
-        <input class="reply-input" type="text" placeholder="댓글 달기..." />
-        <button class="reply-btn">게시</button>
+    <div className="feed-comment">
+      <ul className="comment-box">
+        <InputComments userInput={comment} />
+      </ul>
+      <form className="reply">
+        <input
+          className="reply-input"
+          type="text"
+          placeholder="댓글 달기..."
+          onChange={handleCommentInput}
+          onKeyUp={upLoadBtnValidation}
+        />
+        <button
+          className={isUploadBtn ? 'btn-change' : 'reply-btn'}
+          disabled={!isUploadBtn}
+          onClick={uploadComment}
+        >
+          게시
+        </button>
       </form>
     </div>
   );
